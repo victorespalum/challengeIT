4af8735a6bb9024401ecfb3907851d29542ebe3f
Victor espejo contreras <73442380+victorespalum@users.noreply.github.com>
2022-12-05 22:45:46 +0100

    Versión 0.5.0.4

--- models/User.js
+++ models/User.js
@@ -40,6 +40,10 @@ const userSchema = new Schema({
     description: {
         type: String,
         required: true
+    },
+    image: {
+        type: String,
+        required: true
     }
 })
 
